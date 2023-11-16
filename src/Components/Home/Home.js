73f182612d5953d91e2d08007131d6d9c972b94d
haris8121 <harishchinchala@gmail.com>
2023-11-16 14:19:09 +0530

    home changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -914,6 +914,11 @@ function Home() {
           </footer>
         </div>
 
+        <script src="../node_modules/react-refresh/cjs/react-refresh-runtime.production.min.js"></script> 
+
+    <script src="../node_modules/react-refresh/cjs/react-refresh-babel.development.js"></script>
+
+
         <div id="pop-up">
           <div className="table-responsive">
             <table
