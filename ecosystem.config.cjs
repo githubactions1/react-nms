abcf7c8bd2a5614c826f81990bb241d5e79e1eb5
haris8121 <harishchinchala@gmail.com>
2023-11-17 18:22:21 +0530

    index changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: "NMS-React",
+      name: "react_nms",
       script: "npm run start ",
     },
   ],
   env: {
     PM2_RPC_SOCKET_PATH: '/etc/pm2daemon/rpc.sock',
-  }
+  },
+  
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify")
+    }
+  },
 };
 
--- src/index.js
+++ src/index.js
@@ -3,75 +3,80 @@ import ReactDOM from "react-dom";
 import App from './App.js'; 
 
 
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
-import "../node_modules/node-waves/dist/waves.min.css"
-import "../node_modules/simplebar/dist/simplebar.min.css"
-
-import "../node_modules/jquery/dist/jquery.min.js"
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
-// import "assets/js/waves.js" 
-import "../node_modules/node-waves/dist/waves.js"
-// import "assets/js/simplebar.min.js"
-import "../node_modules/simplebar/dist/simplebar.min.js"
-
-// import "../node_modules/@pmmmwh/react-refresh-webpack-plugin/client/ReactRefreshEntry.js"
-
-import "../node_modules/source-map-loader/dist/cjs.js"
-
-import "../node_modules/webpack-dev-server/client/index.js"
-
-import "../node_modules/webpack/hot/dev-server.js"
-
-// import "../node_modules/autonumeric/autoNumeric-min.js"
-// import "../node_modules/popper.js/dist/popper.min.js"
 
 
- import "../node_modules/morris.js/morris.min.js"
 
- import "../node_modules/raphael/raphael.min.js"
+// import "/bootstrap/dist/js/bootstrap.bundle.min.js"
+// import "/node-waves/dist/waves.min.css"
+// import "/simplebar/dist/simplebar.min.css"
+
+// import "/jquery/dist/jquery.min.js"
+// import "/bootstrap/dist/js/bootstrap.bundle.min.js"
+// // import "assets/js/waves.js" 
+// import "/node-waves/dist/waves.js"
+// // import "assets/js/simplebar.min.js"
+// import "/simplebar/dist/simplebar.min.js"
+
+// import "/@pmmmwh/react-refresh-webpack-plugin/client/ReactRefreshEntry.js"
+
+// // import "/source-map-loader/dist/cjs.js"
+
+// // import "/webpack-dev-server/client/index.js"
+
+// // import "/webpack/hot/dev-server.js"
+
+// // import "/autonumeric/autoNumeric-min.js"
+
+
+// // import "/popper.js/dist/popper.min.js"
+
+
+// //  import "/morris.js/morris.min.js"
+
+//  import "/raphael/raphael.min.js"
  
  
-//  import "assets/pages/dashboard-demo.js"
+// //  import "assets/pages/dashboard-demo.js"
 
-//  import "../node_modules/popper.js/dist/popper.js"
+// //  import "/popper.js/dist/popper.js"
 
-// import "../node_modules/popper.js/dist/popper.min.js"
+// // import "/popper.js/dist/popper.min.js"
  
  
-//  import "assets/js/theme.js" 
+// //  import "assets/js/theme.js" 
 
- import "../node_modules/bootstrap-maxlength/dist/bootstrap-maxlength.min.js"
+//  import "/bootstrap-maxlength/dist/bootstrap-maxlength.min.js"
 
- import "../node_modules/multiselect-react-dropdown/dist/multiselect-react-dropdown.esm.js"
+//  import "/multiselect-react-dropdown/dist/multiselect-react-dropdown.esm.js"
 
- import "../node_modules/react-tooltip/dist/react-tooltip.umd.min.js"
-
-
- import "../node_modules/mohithg-switchery/dist/switchery.js"
-
- import "../node_modules/bootstrap-select/dist/css/bootstrap-select.min.css"
+//  import "/react-tooltip/dist/react-tooltip.umd.min.js"
 
 
- import "../node_modules/bootstrap-icons/bootstrap-icons.svg"
+//  import "/mohithg-switchery/dist/switchery.js"
+
+//  import "/bootstrap-select/dist/css/bootstrap-select.min.css"
 
 
- 
+//  import "/bootstrap-icons/bootstrap-icons.svg"
+
 
  
 
-//  import "assets/pages/morris-chart-demo.js"
  
 
- import "../node_modules/chart.js/dist/chart.js"
+// //  import "assets/pages/morris-chart-demo.js"
+ 
 
-//  import "public/assets/chartjs-demo.js"
+//  import "/chart.js/dist/chart.js"
+
+// //  import "public/assets/chartjs-demo.js"
 
 
 
-// import 'bootstrap/'
-// import './js/theme.js'
+// // import 'bootstrap/'
+// // import './js/theme.js'
 
-import "../node_modules/webpack/lib/Compilation.js"
+// import "/webpack/lib/Compilation.js"
 
 
 
