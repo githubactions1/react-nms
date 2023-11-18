4434781e6b208a266d9d369d5f267e4741ebd186
haris8121 <harishchinchala@gmail.com>
2023-11-18 19:00:32 +0530

    craco

--- src/index.js
+++ src/index.js
@@ -1,89 +1,31 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import App from './App.js'; 
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import $ from  'jquery';
 
+// Bootstrap and related dependencies
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-maxlength/dist/bootstrap-maxlength.min.js';
 
+// Other UI Enhancements
+import 'node-waves/dist/waves.min.css';
+// import './js/waves.js';
+import 'simplebar/dist/simplebar.min.css';
+// import 'assets/js/simplebar.min.js';
+import 'multiselect-react-dropdown/dist/multiselect-react-dropdown.esm.js';
+// import 'react-tooltip/dist/react-tooltip.umd.min.js';
 
+// Charting and graphics
+import 'morris.js/morris.min.js';
+import 'raphael/raphael.min.js';
 
+// Input handling and formatting
+import 'jquery/dist/jquery.min.js';
+// import 'autonumeric/autoNumeric-min.js';
 
-
-
-// import "/bootstrap/dist/js/bootstrap.bundle.min.js"
-// import "/node-waves/dist/waves.min.css"
-// import "/simplebar/dist/simplebar.min.css"
-
-// import "/jquery/dist/jquery.min.js"
-// import "/bootstrap/dist/js/bootstrap.bundle.min.js"
-// // import "assets/js/waves.js" 
-// import "/node-waves/dist/waves.js"
-// // import "assets/js/simplebar.min.js"
-// import "/simplebar/dist/simplebar.min.js"
-
-// import "/@pmmmwh/react-refresh-webpack-plugin/client/ReactRefreshEntry.js"
-
-// // import "/source-map-loader/dist/cjs.js"
-
-// // import "/webpack-dev-server/client/index.js"
-
-// // import "/webpack/hot/dev-server.js"
-
-// // import "/autonumeric/autoNumeric-min.js"
-
-
-// // import "/popper.js/dist/popper.min.js"
-
-
-// //  import "/morris.js/morris.min.js"
-
-//  import "/raphael/raphael.min.js"
- 
- 
-// //  import "assets/pages/dashboard-demo.js"
-
-// //  import "/popper.js/dist/popper.js"
-
-// // import "/popper.js/dist/popper.min.js"
- 
- 
-// //  import "assets/js/theme.js" 
-
-//  import "/bootstrap-maxlength/dist/bootstrap-maxlength.min.js"
-
-//  import "/multiselect-react-dropdown/dist/multiselect-react-dropdown.esm.js"
-
-//  import "/react-tooltip/dist/react-tooltip.umd.min.js"
-
-
-//  import "/mohithg-switchery/dist/switchery.js"
-
-//  import "/bootstrap-select/dist/css/bootstrap-select.min.css"
-
-
-//  import "/bootstrap-icons/bootstrap-icons.svg"
-
-
- 
-
- 
-
-// //  import "assets/pages/morris-chart-demo.js"
- 
-
-//  import "/chart.js/dist/chart.js"
-
-// //  import "public/assets/chartjs-demo.js"
-
-
-
-// // import 'bootstrap/'
-// // import './js/theme.js'
-
-// import "/webpack/lib/Compilation.js"
-
-
-
-
-
+// React-specific components
+import 'popper.js/dist/popper.min.js';
 
 ReactDOM.render(
   <React.StrictMode>
